a71a83c312e6e0f748131512b5dd3fc710c052e8
federicopivetta <101715346+federicopivetta@users.noreply.github.com>
2024-01-28 15:14:17 +0100

    error pages , chiSiamo

--- connection.php
+++ connection.php
@@ -26,133 +26,111 @@ class DBAccess {
         }
     }
 
+    // MODIFICA IL DATABASE
+    public function modifyDatabase($query) {
 
+        $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
+        return $result;
+    }
+
+
+
+
+
+    // PAGINA LOGIN
     public function getPeopleId($id) {
         $tuples = array();
     
-        // Query per ottenere le categorie dalla tabella 'categoria'
         $query ="SELECT id,amministratore FROM utente WHERE id='$id'";
         $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
     
         if ($result && $result->num_rows > 0) {
-            // Fetch dei dati e inserimento nell'array $categories
             while ($row = $result->fetch_assoc()) {
                 $tuples[] = $row;
             }
     
-            // Libera la memoria del risultato
             $result->free_result();
         } else {
-            // Se la query non ha prodotto risultati o ha fallito, gestisci il caso vuoto
-            // Puoi impostare $categories su un valore predefinito o fare altre operazioni necessarie.
-            // Ad esempio, impostare $categories su un array vuoto:
             $tuples= array();
         }
-    
         return $tuples;
     }
-//PAGINA LOGIN controllo se loggato e admin
-function isLoggedInAdmin(){
 
-    if(isset($_SESSION['user'])){
+    function isLoggedInAdmin(){
 
-        $connection = new DBAccess();
+        if(isset($_SESSION['user'])){
 
-        if($connection->openDBConnection()){
+            $connection = new DBAccess();
 
-            $id = $_SESSION['user'];
+            if($connection->openDBConnection()){
 
-            $users = $connection->getPeopleId($id);
+                $id = $_SESSION['user'];
+                $users = $connection->getPeopleId($id);
 
-            if(count($users)>0){
-
-                $user = $users[0];
-                return $user['amministratore']==1; //ritorna true se esiste ed è admin
+                if(count($users)>0){
+                    $user = $users[0];
+                    return $user['amministratore']==1; //ritorna true se esiste ed è admin
+                }else{
+                    return false; //utente non esiste
+                }
             }else{
-                return false; //utente non esiste
+                return false; //non è possibile collegarsi al DB
             }
-
         }else{
-            return false; //non è possibile collegarsi al DB
+            return false; //nessuna sessione attiva
         }
-
-    }else{
-        return false; //nessuna sessione attiva
     }
 
-}
+    function isLoggedInUser(){
 
-//controllo se loggato e utente normale
-function isLoggedInUser(){
+        if(isset($_SESSION['user'])){
 
-    if(isset($_SESSION['user'])){
+            $connection = new DBAccess();
 
-        $connection = new DBAccess();
+            if($connection->openDBConnection()){
 
-        if($connection->openDBConnection()){
+                $id = $_SESSION['user'];
+                $users = $connection->getPeopleId($id);
 
-            $id = $_SESSION['user'];
-
-            $users = $connection->getPeopleId($id);
-
-            if(count($users)>0){
-
-                $user = $users[0];
-               
-                return $user['amministratore']==0; //ritorna true se è user
+                if(count($users)>0){
+                    $user = $users[0];
+                    return $user['amministratore']==0; //ritorna true se esiste ed è user
+                }else{
+                    return false; //utente non esiste
+                }
             }else{
-                return false; //utente non esiste
+                return false; //non è possibile collegarsi al DB
             }
-
         }else{
-            return false; //non è possibile collegarsi al DB
+            return false; //nessuna sessione attiva
         }
 
-    }else{
-        return false; //nessuna sessione attiva
     }
 
-}
-//trovare admin nel database
-public function getRowsFromDatabase($nome) {
+    public function getRowsFromDatabase($nome) {
     $people = array();
 
-    // Query per ottenere le categorie dalla tabella 'categoria'
     $query ="SELECT id,passw,amministratore FROM utente WHERE nome='$nome'";
     $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
     if ($result && $result->num_rows > 0) {
-        // Fetch dei dati e inserimento nell'array $categories
         while ($row = $result->fetch_assoc()) {
             $people[] = $row;
         }
-
-        // Libera la memoria del risultato
         $result->free_result();
     } else {
-        // Se la query non ha prodotto risultati o ha fallito, gestisci il caso vuoto
-        // Puoi impostare $categories su un valore predefinito o fare altre operazioni necessarie.
-        // Ad esempio, impostare $categories su un array vuoto:
         $people= array();
     }
-
     return $people;
-}
-
-//INSERIMENTO NEL DATABASE GENERALE, VA BENE SIA PER REGISTRAZIONE CHE PER FAQ E PRODOTTO
-
-public function modifyDatabase($query) {
-
-    $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
-    return $result;
-}
+    }
 
 
 
 
 
 
-//PAGINA HOME
+
+    // PAGINA HOME
     public function getCategoriesFromDatabase() {
         $categories = array();
     
@@ -161,45 +139,30 @@ public function modifyDatabase($query) {
         $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
     
         if ($result && $result->num_rows > 0) {
-            // Fetch dei dati e inserimento nell'array $categories
             while ($row = $result->fetch_assoc()) {
                 $categories[] = $row;
             }
-    
-            // Libera la memoria del risultato
-            $result->free_result();
+            $result->free_result(); // Libera la memoria del risultato
         } else {
-            // Se la query non ha prodotto risultati o ha fallito, gestisci il caso vuoto
-            // Puoi impostare $categories su un valore predefinito o fare altre operazioni necessarie.
-            // Ad esempio, impostare $categories su un array vuoto:
             $categories = array();
         }
-    
         return $categories;
     }
 
     public function getBrandsFromDatabase() {
         $brands = array();
     
-        // Query per ottenere le categorie dalla tabella 'Brands'
         $query = "SELECT * FROM marca";
         $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
     
         if ($result && $result->num_rows > 0) {
-            // Fetch dei dati e inserimento nell'array $brands
             while ($row = $result->fetch_assoc()) {
                 $brands[] = $row;
             }
-    
-            // Libera la memoria del risultato
             $result->free_result();
         } else {
-            // Se la query non ha prodotto risultati o ha fallito, gestisci il caso vuoto
-            // Puoi impostare $brands su un valore predefinito o fare altre operazioni necessarie.
-            // Ad esempio, impostare $categories su un array vuoto:
             $brands = array();
         }
-    
         return $brands;
     }
 
@@ -207,150 +170,123 @@ public function modifyDatabase($query) {
 
 
 
-//PAGINA ACCESSORI
-public function getAccessoriFromDatabase() {
+    // PAGINA ACCESSORI
+    public function getAccessoriFromDatabase() {
     $accessori = array();
 
     $query = "SELECT ID,nome,immagine1,prezzo FROM prodotto WHERE categoria=1";
     $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
     if ($result && $result->num_rows > 0) {
-        // Fetch dei dati e inserimento nell'array $accessori
         while ($row = $result->fetch_assoc()) {
             $accessori[] = $row;
         }
-
-        // Libera la memoria del risultato
         $result->free_result();
     } else {
-
         $accessori = array();
     }
-
     return $accessori;
-}
+    }
 
-//PAGINA PESI LIBERI
-public function getPesiLiberiFromDatabase() {
+    //PAGINA PESI LIBERI
+    public function getPesiLiberiFromDatabase() {
     $pesiLiberi = array();
 
     $query = "SELECT ID,nome,immagine1,prezzo FROM prodotto WHERE categoria=2";
     $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
     if ($result && $result->num_rows > 0) {
-        // Fetch dei dati e inserimento nell'array $pesiLiberi
         while ($row = $result->fetch_assoc()) {
             $pesiLiberi[] = $row;
         }
-
-        // Libera la memoria del risultato
         $result->free_result();
     } else {
-
         $pesiLiberi = array();
     }
-
     return $pesiLiberi;
-}
+    }
 
-//PAGINA NUTRIZIONE
-public function getNutrizioneFromDatabase() {
-    $Nutrizione = array();
+    // PAGINA NUTRIZIONE
+    public function getNutrizioneFromDatabase() {
+    $nutrizione = array();
 
     $query = "SELECT ID,nome,immagine1,prezzo FROM prodotto WHERE categoria=3";
     $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
     if ($result && $result->num_rows > 0) {
-        // Fetch dei dati e inserimento nell'array $Nutrizione
         while ($row = $result->fetch_assoc()) {
-            $Nutrizione[] = $row;
+            $nutrizione[] = $row;
         }
-
-        // Libera la memoria del risultato
         $result->free_result();
     } else {
-
-        $Nutrizione = array();
+        $nutrizione = array();
+    }
+    return $nutrizione;
     }
 
-    return $Nutrizione;
-}
-
-//PAGINA MACCHINE
-public function getMacchineFromDatabase() {
-    $Macchine = array();
+    // PAGINA MACCHINARI
+    public function getMacchinariFromDatabase() {
+    $macchinari = array();
 
     $query = "SELECT ID,nome,immagine1,prezzo FROM prodotto WHERE categoria=4";
     $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
     if ($result && $result->num_rows > 0) {
-        // Fetch dei dati e inserimento nell'array $Macchine
         while ($row = $result->fetch_assoc()) {
-            $Macchine[] = $row;
+            $macchinari[] = $row;
         }
-
-        // Libera la memoria del risultato
         $result->free_result();
     } else {
-
-        $Macchine = array();
+        $macchinari = array();
+    }
+    return $macchinari;
     }
 
-    return $Macchine;
-}
 
 
 
 
-
-
-
-//PAGINA FAQ
-//variabile di input con ID dell'utente?
-public function getFaqFromDataBase() {
+    //PAGINA FAQ
+    public function getFaqFromDataBase() {
     $domande_risposta = array();
 
-    $query = "SELECT domanda, risposta FROM faq WHERE utente=1";//cambiare
+    $query = "SELECT domanda, risposta FROM faq WHERE utente=1";// cambiare con id utente
     $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
     if ($result && $result->num_rows > 0) {
-        // Fetch dei dati e inserimento nell'array $domande
         while ($row = $result->fetch_assoc()) {
             $domande_risposta[] = $row;
         }
-
-
         $result->free_result();
     } else {
-
         $domande_risposta = array();
     }
-
     return $domande_risposta;
-}
+    }
 
-public function getQuestionsFromDataBaseForAdmin() {
+    public function getQuestionsFromDataBaseForAdmin() {
     $domande_risposta = array();
+
     $query = "SELECT ID, domanda, risposta FROM faq WHERE risposta IS NULL";
     $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
     if ($result && $result->num_rows > 0) {
-        
         while ($row = $result->fetch_assoc()) {
             $domande_risposta[] = $row;
         }
-
         $result->free_result();
     } else {
         $domande_risposta = array();
     }
     return $domande_risposta;
-}
+    }
 
 
 
-//PAGINA FAQ
-//salvare funzione nel DB
+    
+
+
+//usare modifyDB
 public function saveQuestionToDatabase($question, $userId) {
     $question = mysqli_real_escape_string($this->connection, $question);
 
@@ -367,7 +303,8 @@ public function saveQuestionToDatabase($question, $userId) {
     }
 }
 
-public function getProdottoById($idProdotto) {
+    // controllare funzione
+    public function getProdottoById($idProdotto) {
     try {
         $query = "SELECT * FROM `prodotto` WHERE `ID` = ?";
         $stmt = $this->connection->prepare($query);
@@ -395,6 +332,7 @@ public function getProdottoById($idProdotto) {
     }
 }
 
+// esisterebbe gia un get categoria che prende *
 public function getCategoriaFromId($idCategoria) {
     $query = "SELECT nome FROM categoria WHERE ID = $idCategoria";
     $result = $this->connection->query($query);
@@ -414,7 +352,8 @@ public function getCategoriaFromId($idCategoria) {
 
     return $categoria;
 }
- 
+
+// modificare
 public function saveToCart($user_Id, $product_Id, $quantity_Id) {
     // Esegue l'escape delle variabili per evitare SQL injection
     $userId = mysqli_real_escape_string($this->connection, $user_Id);
@@ -452,6 +391,7 @@ public function saveToCart($user_Id, $product_Id, $quantity_Id) {
     }
 }
 
+// modificare
 public function updateProductQuantity($product_Id, $quantity) {
     // Esegue l'escape delle variabili per evitare SQL injection
     $productId = mysqli_real_escape_string($this->connection, $product_Id);
@@ -550,79 +490,67 @@ VALUES ('$nuovo_id', '$nuovo_nome', '$immagine1Path', '$immagine2Path', '$immagi
 
 
 
-//PAGINA CARRELLO
-public function getRiepilogoFromDatabase($id) {
-    $prezzo = 0;
-    $quantita = 0;
+    // PAGINA CARRELLO
+    public function getRiepilogoFromDatabase($id) {
+        $prezzo = 0;
+        $quantita = 0;
 
-    $query1 = "CREATE VIEW prezzoTotale AS
-    SELECT carrello.IDprodotto AS idProdotto, prodotto.prezzo * carrello.quantita AS prezzoTotaleProdotto
-    FROM carrello
-    JOIN prodotto ON carrello.IDprodotto = prodotto.ID";
+        $query1 = "CREATE VIEW prezzoTotale AS
+        SELECT carrello.IDprodotto AS idProdotto, prodotto.prezzo * carrello.quantita AS prezzoTotaleProdotto
+        FROM carrello
+        JOIN prodotto ON carrello.IDprodotto = prodotto.ID";
 
-    $query2 = "SELECT CAST(SUM(prezzoTotale.prezzoTotaleProdotto) AS SIGNED) AS prezzoTotaleUtente
-    FROM carrello
-    JOIN prezzoTotale ON carrello.IDprodotto = prezzoTotale.idProdotto
-    WHERE carrello.IDutente = $id
-    GROUP BY carrello.IDutente";
+        $query2 = "SELECT CAST(SUM(prezzoTotale.prezzoTotaleProdotto) AS SIGNED) AS prezzoTotaleUtente
+        FROM carrello
+        JOIN prezzoTotale ON carrello.IDprodotto = prezzoTotale.idProdotto
+        WHERE carrello.IDutente = $id
+        GROUP BY carrello.IDutente";
 
-    $query3 = "DROP VIEW prezzoTotale";
-
-    // Esegui le query separatamente
+        $query3 = "DROP VIEW prezzoTotale";
     
-    $queryQuantita = "SELECT SUM(quantita) FROM carrello WHERE IDutente= $id";
+        $queryQuantita = "SELECT SUM(quantita) FROM carrello WHERE IDutente= $id";
 
-    $result1 = mysqli_query($this->connection, $query1) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
-    $result2 = mysqli_query($this->connection, $query2) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
-    $result3 = mysqli_query($this->connection, $query3) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
+        $result1 = mysqli_query($this->connection, $query1) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
+        $result2 = mysqli_query($this->connection, $query2) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
+        $result3 = mysqli_query($this->connection, $query3) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
     
-    $resultQuantita = mysqli_query($this->connection, $queryQuantita) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
+        $resultQuantita = mysqli_query($this->connection, $queryQuantita) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
-    if ($result2 && $result2->num_rows > 0 && $resultQuantita && $resultQuantita->num_rows > 0) {
-    
-        $prezzo = $result2->fetch_assoc();
-        $quantita = $resultQuantita->fetch_assoc();
+            if ($result2 && $result2->num_rows > 0 && $resultQuantita && $resultQuantita->num_rows > 0) {
+                $prezzo = $result2->fetch_assoc();
+                $quantita = $resultQuantita->fetch_assoc();
 
-        // Libera la memoria del risultato
-        //$result1->free_result();
-        $result2->free_result();
-        //$result3->free_result();
-        $resultQuantita->free_result();
+                $result2->free_result();
+                $resultQuantita->free_result();
+            }
+        return array($prezzo, $quantita);
     }
-    return array($prezzo, $quantita);
-}
     
-
-
-
-public function getProdottiCarrello($id){
+    public function getProdottiCarrello($id){
     $prodotti = array();
 
     $query = "SELECT IDprodotto,nome,prezzo,carrello.quantita,immagine1 FROM carrello,prodotto WHERE idUtente=$id AND IDprodotto=ID"; //mettere anche alt quando lo metteremo nel db
     $result = mysqli_query($this->connection, $query) or die("Errore nell'accesso al database" .mysqli_error($this->connection));
 
     if ($result && $result->num_rows > 0) {
-        // Fetch dei dati e inserimento nell'array $prodotti
         while ($row = $result->fetch_assoc()) {
             $prodotti[] = $row;
         }
-
-        // Libera la memoria del risultato
         $result->free_result();
     } else {
-
         $prodotti = array();
     }
-
     return $prodotti;
-}
-
-
-public function closeDBConnection(){
-    if($this->connection != null){
-        mysqli_close($this->connection);
     }
-}
+
+
+
+    // CHIUDI CONNESSIONE DB
+    public function closeDBConnection(){
+        if($this->connection != null){
+            mysqli_close($this->connection);
+        }
+    }
     
 }
 ?>
